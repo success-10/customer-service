06833734a0a6b3aae25861d31518ee9c9c6fdae1
Success Adako <successadako@gmail.com>
2025-11-11 12:12:39 +0100

    debug

--- cs_messaging/settings.py
+++ cs_messaging/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["customer-service-lt53.onrender.com",  # my Render URL
     "127.0.0.1",
